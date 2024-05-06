6d956bacbd7ebdee702b4d00eebe3349f231dca4
Yuga Umemura <yugaumemura@Macbook-Air.local>
2024-05-07 00:04:44 +0900

    feat: add GraphQL module with ApolloDriver to AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,17 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      // GraphQL playgroundを使うかどうかの設定
+      playground: true,
+      // schemaファイルのパスを指定
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
 })
 export class AppModule {}
