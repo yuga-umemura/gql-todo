2d2bcb1c8ad92c1bca6ff4af004759d6d2c8df36
Yuga Umemura <yugaumemura@Macbook-Air.local>
2024-06-08 17:15:50 +0900

    update: enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:5173', // frontendのURL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
